5baac5c81c6327944c47f2135281f2567b9e24a8
Josefa <jmendezg@udd.cl>
2022-01-31 22:23:35 -0300

    login falta diagramar dispositivos

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="formulario">
+    <h1>Iniciar Sesión</h1>
+    <v-text-field
+      label="Usuario"
+      outlined
+      placeholder="Correo electrónico"
+    ></v-text-field>
+    <v-text-field label="Contraseña" outlined></v-text-field>
+    <v-btn elevation="2" class="">Iniciar Sesión</v-btn>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+button.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.elevation-2.v-size--default {
+  background-color: #ffc107;
+}
+
+.formulario {
+  margin: auto;
+  width: 60%;
+}
+
+h1 {
+  font-family: "Open Sans", sans-serif;
+  margin: 20px 0;
+}
+
+@media screen and (max-width: 600px) {
+  button.v-btn.v-btn--is-elevated.v-btn--has-bg.theme--light.elevation-2.v-size--default {
+    width: 100%;
+    margin-bottom: 25px;
+  }
+
+  .formulario {
+    width: 95%;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,56 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 // import { observador } from '../firebase/auth.js'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: '/menu',
-    name: 'Menu',
-    component: () => import('../views/Menu.vue')
+    path: "/menu",
+    name: "Menu",
+    component: () => import("../views/Menu.vue"),
   },
   {
-    path: '/users',
-    name: 'Users',
-    component: () => import('../views/Users.vue')
+    path: "/users",
+    name: "Users",
+    component: () => import("../views/Users.vue"),
   },
   {
-    path: '/qr',
-    name: 'QR',
-    component: () => import('../views/QR.vue')
-  }
-]
+    path: "/qr",
+    name: "QR",
+    component: () => import("../views/QR.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
 //Usar el observador cuando se tenga el login y logout creados
 // router.beforeEach((to, from, next) => {
 //   observador(to, next);
 // });
 
-
-export default router
+export default router;
