b1791ca3f0e9a52c46818362e195fbf094a4ec2a
Checho <sergio.avila.astorga@gmail.com>
2022-02-01 21:49:55 -0300

    add scroll in menu

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-
+import VueSmoothScroll from 'vue2-smooth-scroll'
+Vue.use(VueSmoothScroll)
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,120 +1,174 @@
 <template>
-    <div>
-        <div class="header">
-            <h1>mesapp</h1>
-        </div>
-        <div class="section" v-for="(index) in cateogiries" :key="index">
-            <img src="/img/destacados.png" alt="Destacados" class="bg">
-            <v-container>
-                <h2>Destacados</h2>
-                <v-row class="cards">
-                    <v-col cols="12" lg="6" v-for="(index) in plates" :key="index" :align-self="index == 1 ? 'stretch' : ''">
-                        <v-card elevation="0" outlined>
-                            <v-layout>
-                                <v-flex xs4>
-                                <v-img src="/img/sandwich.jpg"></v-img>
-                                </v-flex>
-                                <v-flex xs8>
-                                <v-card-title primary-title> 
-                                    Text on the right
-                                    <span class="price">$9.990</span>
-                                </v-card-title>
-                                <v-card-text>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil magni sint explicabo.
-                                </v-card-text>
-                                </v-flex>
-                            </v-layout>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-container>
-
-        </div>
+  <div>
+    <div class="header">
+      <h1>mesapp</h1>
+      <v-flex class="menu">
+        <a href="#id-1" v-smooth-scroll="{ duration: 500, offset: -125 }"
+          >Categoría</a
+        >
+        <a href="#id-2" v-smooth-scroll="{ duration: 500, offset: -125 }"
+          >Categoría</a
+        >
+        <a href="#id-3" v-smooth-scroll="{ duration: 500, offset: -125 }"
+          >Categoría</a
+        >
+        <a href="#id-4" v-smooth-scroll="{ duration: 500, offset: -125 }"
+          >Categoría</a
+        >
+      </v-flex>
     </div>
+
+    <span class="separator"></span>
+    <div class="section" v-for="index in cateogiries" :key="index">
+      <img src="/img/destacados.png" alt="Destacados" class="bg" />
+      <div class="container" :id="`id-${index}`">
+        <h2>Destacados</h2>
+        <v-row class="cards">
+          <v-col cols="12" lg="6" v-for="index in plates" :key="index">
+            <v-card elevation="0" outlined>
+              <v-layout>
+                <v-flex xs4>
+                  <v-img src="/img/sandwich.jpg"></v-img>
+                </v-flex>
+                <v-flex xs8>
+                  <v-card-title primary-title>
+                    Text on the right
+                    <span class="price">$9.990</span>
+                  </v-card-title>
+                  <v-card-text>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Nihil magni sint explicabo.
+                  </v-card-text>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-      return{
-          cateogiries: [1,2,3],
-          plates: [1,2,3,4,5,6,7,8]
-      }  
-    },
-}
+  data() {
+    return {
+      cateogiries: [1, 2, 3],
+      plates: [1, 2, 3, 4, 5, 6, 7, 8],
+    };
+  },
+  methods: {},
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Satisfy&display=swap');
-.v-main{
-    overflow-x: hidden;
+@import url("https://fonts.googleapis.com/css2?family=Satisfy&display=swap");
+.v-main {
+  overflow-x: hidden;
 }
-.header{
-    background-image: url('/img/bg-coffee.jpg');
-    background-size: cover;
-    background-position: center;
+.menu {
+  width: 100%;
+  background: white;
+  overflow-x: scroll;
+  display: flex;
+  box-shadow: 0px 5px 12px -5px #000000;
+  height: 60px;
+}
+.menu::-webkit-scrollbar {
+  display: none;
+}
+.menu a {
+  padding: 10px 18px;
+  text-decoration: none;
+  color: #555;
+  font-size: 1.2em;
+}
+.header {
+  background-image: url("/img/bg-coffee.jpg");
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 300px;
+  display: flex;
+  align-items: flex-start;
+  color: white;
+  flex-direction: column;
+  display: block;
+  z-index: 9;
+  position: fixed;
+}
+.header h1 {
+  padding: 3em 1em;
+}
+.separator {
     height: 300px;
-    display: flex;
-    align-items: flex-end;
-    color: white;
-}
-.header h1{
-    padding: 1em;
-}
-h2{
-    font-family: 'Satisfy', cursive;
-    font-size: 3em;
-    background-image: url('/img/bg-titles.png');
-    background-repeat: no-repeat;
-    padding-left: 1em;
-    margin: 1em 0;
+    display: block;
+  }
+h2 {
+  font-family: "Satisfy", cursive;
+  font-size: 3em;
+  background-image: url("/img/bg-titles.png");
+  background-repeat: no-repeat;
+  padding-left: 1em;
+  margin: 1em 0;
 }
 .section {
-    position: relative;
+  position: relative;
 }
-.section .bg{
-    position: absolute;
-    right: 0;
-    top: 0;
-    z-index: 0;
+.section .bg {
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 0;
 }
-.v-sheet.v-card{
-    border-radius: 10px !important;
-    border-color: #8A5D3B;
+.v-sheet.v-card {
+  border-radius: 10px !important;
+  border-color: #8a5d3b;
 }
-.v-card__title{
-    color: #8A5D3B;
-    justify-content: space-between;
+.v-card__title {
+  color: #8a5d3b;
+  justify-content: space-between;
 }
-.v-card__title .price{
-    background: #8A5D3B;
-    color: white;
-    border-radius: 8px;
-    padding: 0 10px;
+.v-card__title .price {
+  background: #8a5d3b;
+  color: white;
+  border-radius: 8px;
+  padding: 0 10px;
 }
-.theme--light.v-image{
-    height: 100%;
-    -webkit-border-top-left-radius: 10px;
-    -webkit-border-bottom-left-radius: 10px;
-    -moz-border-radius-topleft: 10px;
-    -moz-border-radius-bottomleft: 10px;
-    border-top-left-radius: 10px;
-    border-bottom-left-radius: 10px;
+.theme--light.v-image {
+  height: 100%;
+  -webkit-border-top-left-radius: 10px;
+  -webkit-border-bottom-left-radius: 10px;
+  -moz-border-radius-topleft: 10px;
+  -moz-border-radius-bottomleft: 10px;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
 }
-@media (max-width: 480px){
-    .header{
-        height: 200px;
-    }
-    .section .bg{
-        width: 45%;
-        right: -10%;
-        top: 2%;
-    }
-    h2{
-        padding-left: 0.6em;
-    }
-    .v-card__title{
-        justify-content: flex-end;
-    }
+@media (max-width: 480px) {
+  .header {
+    height: 150px;
+    width: 100%;
+    display: block;
+    z-index: 9;
+    position: fixed;
+  }
+  .header h1 {
+    padding: 1em;
+}
+  .separator {
+    height: 150px;
+    display: block;
+  }
+  .section .bg {
+    width: 45%;
+    right: -10%;
+    top: 2%;
+  }
+  h2 {
+    padding-left: 0.6em;
+  }
+  .v-card__title {
+    justify-content: flex-end;
+  }
 }
 </style>
